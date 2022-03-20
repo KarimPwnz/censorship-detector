b380e28256f11bff9a846c1868836c984db7d9e8
Karim Rahal <karim@karimrahal.com>
2022-03-19 17:04:33 -0700

    Document Checker

--- src/js/detector/checker.js
+++ src/js/detector/checker.js
@@ -1,9 +1,22 @@
+/**
+ * A Checker that runs checks and caches their results
+ * 
+ * @param {Context} context - Context to run checks in
+ */
 export default class Checker {
     constructor(context) {
         this.context = context;
         this.cacheStore = new Map();
     }
 
+    /**
+     * Run a check
+     * 
+     * @param {Check} check - check to run
+     * @param {webRequestEvent} event - event to run check on
+     * @param {boolean} [bypassCache=false]
+     * @returns 
+     */
     check(check, event, { bypassCache = false } = {}) {
         let cachedCheck = this.cacheStore.get(check.meta.name) || {};
         // Check if bypass cache or cache does not exist
